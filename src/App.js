6d602ede225b1293c5e904af69c1a35bee928852
AniketYadav1999 <aniketyadav610@gmail.com>
2024-02-09 18:37:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Front from './Components/Front';
+import './Components/Front.css';
+function App() {
+  return (
+       <Front/>
+  );
+}
+
+export default App;
